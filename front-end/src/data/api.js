671ff839cd94fddc8b2e110f766b4c71a2f986e7
armantai732 <taimunna722@gmail.com>
2026-09-01 12:39:59 +0530

    changes url

--- front-end/src/data/api.js
+++ front-end/src/data/api.js
@@ -1,4 +1,4 @@
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8080"
+const API_URL = import.meta.env.VITE_API_URL"
 const BASE_URL = `${API_URL}/api`
 
 const authHeaders = () => {
