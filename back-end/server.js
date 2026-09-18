68269608a23158b0d987ee9e824d9187f1ad348d
armantai732 <taimunna722@gmail.com>
2026-09-18 19:41:43 +0530

    change

--- back-end/server.js
+++ back-end/server.js
@@ -12,7 +12,7 @@ app.use(
     cors({
         origin: [
             "http://localhost:5173",
-            "https://king-fashion-three.vercel.app/",
+            "https://king-fashion-three.vercel.app",
         ]
     })
 );
